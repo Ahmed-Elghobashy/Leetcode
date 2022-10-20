2c29f60a2a2c4020567b7cb74847f6cfb58eea7f
Ahmed El Ghobashy <61662872+Ahmed-Elghobashy@users.noreply.github.com>
2022-10-20 13:09:17 +0200

    Time: 4 ms (50.80%), Space: 47.4 MB (38.31%) - LeetHub

---
+++ 0099-recover-binary-search-tree/0099-recover-binary-search-tree.java
@@ -0,0 +1,31 @@
+class Solution {
+    TreeNode firstElement = null;
+    TreeNode secondElement = null;
+    TreeNode prevElement = null;
+
+    public void recoverTree(TreeNode root) {
+        traverse(root);
+
+        int temp = firstElement.val;
+        firstElement.val = secondElement.val;
+        secondElement.val = temp;
+    }
+
+    private void traverse(TreeNode root) {
+        if (root == null) return;
+
+        traverse(root.left);
+
+        if (prevElement != null) {
+            if (firstElement == null && prevElement.val >= root.val) {
+                firstElement = prevElement;
+            }
+        }
+        if (firstElement != null && prevElement.val >= root.val) {
+            secondElement = root;
+        }
+        prevElement = root;
+
+        traverse(root.right);
+    }
+}
